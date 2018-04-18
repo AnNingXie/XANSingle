b322aa34bee93951aa157f3a2595e11b7d456caf
xieanning <15003876305@163.com>
2018-04-18 16:48:45 +0800

    初始化项目

---
+++ XANSingle/XANSingle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XANSingle
+//
+//  Created by XAN on 2018/4/18.
+//  Copyright © 2018年 xieanning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XANSingle/XANSingle/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XANSingle
+//
+//  Created by XAN on 2018/4/18.
+//  Copyright © 2018年 xieanning. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
